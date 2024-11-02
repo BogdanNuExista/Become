bec9d6e088b2deb52f7b0924fce5e92bb3b43a40
BogdanNuExista <bogdancraiu15@gmail.com>
2024-11-02 22:00:13 +0200

    fixed a warning

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDpIHp6FFBIWv-yxuyPXuRa6wLynE-FzoA",
@@ -14,7 +16,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+// Initialize Firebase Auth with AsyncStorage persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
 const db = getFirestore(app);
 
 export { app, auth, db };
